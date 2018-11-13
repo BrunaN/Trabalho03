a8d6e25c8babd61925c26e22ead09a11663a52a3
Bruna Neves <brubsnev@gmail.com>
2018-11-13 21:24:10 -0200

    permissão para download

--- Trabalho032/app/src/main/java/com/bn/trabalho_03/MainActivity.java
+++ Trabalho032/app/src/main/java/com/bn/trabalho_03/MainActivity.java
@@ -1,7 +1,9 @@
 package com.bn.trabalho_03;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
@@ -9,11 +11,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -23,6 +30,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CODE = 1;
 
     private View buttonStart;
     private View buttonStop;
@@ -36,6 +45,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= 23)
+        {
+            if (checkPermission()) {
+                // Code for above or equal 23 API Oriented Device
+                // Your Permission granted already .Do next code
+            } else {
+                requestPermission(); // Code for permission
+            }
+        }
+        else
+        {
+            // Code for Below 23 API Oriented Device
+            // Do next code
+        }
 
         downloadStatus = findViewById(R.id.download_status);
         Button btnDownload = findViewById(R.id.buttonDownload);
@@ -62,6 +86,37 @@ public class MainActivity extends AppCompatActivity {
         startService(i);
         InternetNotificationService.mainActivity = this;
 
+    }
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void requestPermission() {
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            Toast.makeText(this, "Write External Storage permission allows us to do store images. Please allow this permission in App Settings.", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.e("value", "Permission Granted, Now you can use local drive .");
+                } else {
+                    Log.e("value", "Permission Denied, You cannot use local drive .");
+                }
+                break;
+        }
     }
 
     public void startPlayer(View view){
@@ -117,18 +172,28 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+
     public void notification(){
 
-        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        //NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        //NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+        //        .setTicker("teste")
+        //       .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+        //        .setContentTitle("Notificação da Bruninha")
+        //         .setContentText("Bruninha => Notificação de conexão com a Internet");
+
+        //Notification notification = mBuilder.build();
+
+        //mNotificationManager.notify(R.drawable.ic_notifications_black_24dp, notification);
+
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
-                .setTicker("teste")
                 .setSmallIcon(R.drawable.ic_notifications_black_24dp)
                 .setContentTitle("Notificação da Bruninha")
-                .setContentText("Bruninha => Notificação de conexão com a Internet");
+                .setContentText("Bruninha => Notificação de conexão com a Internet")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
-        Notification notification = mBuilder.build();
-
-        mNotificationManager.notify(R.drawable.ic_notifications_black_24dp, notification);
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(001, mBuilder.build());
 
     }
 
@@ -142,7 +207,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickFinalizarBindedService( View view ){
-        // Unbind from the service
         if (mBound) {
             unbindService(mConnection);
             mBound = false;
@@ -152,9 +216,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickChamarBindedService( View view ){
         if (mBound) {
-            // Call a method from the LocalService.
-            // However, if this call were something that might hang, then this request should
-            // occur in a separate thread to avoid slowing down the activity performance.
             int num = bindedService.getChange();
 
             TextView textView = findViewById(R.id.textView);
